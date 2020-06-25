b80d13dc3769cb69eb828d1600aeb9da8c1ba560
Dmitry <martynenkodima21@gmail.com>
2020-06-26 01:12:13 +0300

    m1891: Edit device identifier

--- omni_m1891.mk
+++ omni_m1891.mk
@@ -7,8 +7,16 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit language packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_m1891
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m1891
-PRODUCT_BRAND := meizu
-PRODUCT_MANUFACTURER := meizu
+PRODUCT_NAME := omni_m1891
+PRODUCT_BRAND := Meizu
+PRODUCT_MODEL := Meizu 15 Plus
+PRODUCT_MANUFACTURER := Meizu
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="15 Plus" \
+    BUILD_PRODUCT="m1891" \
+    PRIVATE_BUILD_DESC="Meizu_15Plus-user 7.0 NRD90M 1530692351 release-keys"
+	
+BUILD_FINGERPRINT := "Meizu/Meizu_15Plus/15Plus:7.0/NRD90M/1530692351:user/release-keys"
